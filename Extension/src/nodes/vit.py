3ab0327439941bbb84537ee3402d41cfb8e1a749
Alberto de Leo <deleoa@tcd.ie>
2025-02-18 11:22:36 +0000

    Update vit.py

--- Extension/src/nodes/vit.py
+++ Extension/src/nodes/vit.py
@@ -50,43 +50,32 @@ image_category = knext.category(
 )
 class VisionTransformerLearnerNode:
     """
-    Vision Transformer Learner Node
-    ========================================
+    Vision Transformer Learner node
 
-    The Vision Transformer Learner node enables users to fine-tune transformer-based models. 
-    It is a deep learning model that processes image data by dividing it into patches and applying transformer-based 
+    The Vision Transformer Learner node enables users to fine-tune transformer-based models.
+    It is a deep learning model that processes image data by dividing it into patches and applying transformer-based
     self-attention mechanisms.
 
-    The model is fine-tuned using a selected image column and label column from the input training dataset. Users can 
-    choose from multiple Transformer architectures, including ViT, Swin Transformer, and Pyramid Transformer. 
-    Training parameters such as batch size, number of epochs, and learning rate can be customized. After the fine-tunig part, the node takes as input the validation set, 
+    The model is fine-tuned using a selected image column and label column from the input training dataset. Users can
+    choose from multiple Transformer architectures, including ViT, Swin Transformer, and Pyramid Transformer.
+    Training parameters such as batch size, number of epochs, and learning rate can be customized. After the fine-tunig part, the node takes as input the validation set,
     which is helpful to find the best parameters of the model.
     The node outputs a fine-tuned model that can be used for image classification with the Transformer Predictor node.
-    
+
     This node supports fine-tuning of three different models' architectures:
 
 
-    - **Vision Transformer (ViT)**: A transformer-based model for image classification that treats images as sequences of 
-      patches and applies self-attention mechanisms. 
+    - **Vision Transformer (ViT)**: A transformer-based model for image classification that treats images as sequences of
+      patches and applies self-attention mechanisms.
       [More info](https://huggingface.co/docs/transformers/model_doc/vit)
 
-    - **Swin Transformer**: A hierarchical transformer model with shifted window attention, designed for high-resolution 
+    - **Swin Transformer**: A hierarchical transformer model with shifted window attention, designed for high-resolution
       image classification and dense prediction tasks.
       [More info](https://huggingface.co/docs/transformers/model_doc/swin)
 
-    - **Pyramid Vision Transformer (PVT)**: A transformer model that incorporates a pyramid structure with progressively 
+    - **Pyramid Vision Transformer (PVT)**: A transformer model that incorporates a pyramid structure with progressively
       shrinking patch sizes, making it efficient for tasks like object detection and segmentation.
       [More info](https://huggingface.co/docs/transformers/model_doc/pvt)
-
-
-
-    ========================================
-
-
-
-    ### Input Requirements:
-    - **Training Data**: A table containing images and corresponding labels for training the model.
-    - **Validation Data**: A table containing images and labels for validating model performance.
 
     ### Configuration Options:
     - **Image Column**: Select the column containing image data (must be in PNG format).
@@ -96,15 +85,6 @@ class VisionTransformerLearnerNode:
     - **Learning Rate**: Sets the optimizer's step size for updating model weights.
     - **Model Choice**: Choose between ViT, Swin Transformer, or Pyramid Transformer.
 
-    ### Output:
-    - **Fine-tuned Model**: A fully fine-tuned transformer model that can be used for inference.
-    - **Training Summary Table**: A table displaying key training statistics:
-    - Epoch count
-    - Training loss
-    - Training accuracy
-    - Validation loss
-    - Validation accuracy
-
     ### How It Works:
     1. The node processes images and encodes labels.
     2. The selected transformer model is initialized and fine-tuned using the provided training data.
@@ -112,13 +92,7 @@ class VisionTransformerLearnerNode:
     4. Training runs for the specified number of epochs, tracking performance metrics.
     5. The trained model and a performance summary table are returned as outputs.
 
-    This node is ideal for users who want to fine-tune transformer models for image classification tasks without 
-    writing complex deep-learning code. It integrates seamlessly into KNIME's analytics workflow, providing a 
-    user-friendly interface for powerful model training.
-
-
     """
-
 
     image_column = knext.ColumnParameter(
         label="Image Column",
@@ -441,37 +415,21 @@ class ClassificationPredictorGeneralSettings:
 class VisionTransformerPredictor:
 
     """
-    Vision Transformer Predictor   
+    Vision Transformer Predictor
 
-    ========================================
-
-    The Vision Transformer Predictor Node applies a fine-tuned Transformer model to classify images in the given input dataset.
+    The Vision Transformer Predictor node applies a fine-tuned Transformer model to classify images in the given input dataset.
     It computes the predicted class for each image and, optionally, provides class probability estimates.
 
-    The node requires a fine-tuned Transformer model from the Vision Transformer Learner Node and a dataset containing image data.
+    The node requires a fine-tuned Transformer model from the Vision Transformer Learner node and a dataset containing image data.
     The prediction column name can be customized, and the node supports multiple Transformer architectures.
-    
+
     It is only executable if the test data contains the image column that was used by the learner model.
-
-
-    ========================================
-
-
-    ### Input Requirements:
-
-    - **Trained Model**: A ViT model trained using the ViT Classification Learner node.
-    - **Test Input Data**: A table containing image data for classification.
 
     ### Configuration Options:
 
     - **Custom Prediction Column Name**: Specify a custom name for the prediction column.
     - **Predict Probability Estimates**: Enable to obtain confidence scores for each class.
     - **Probability Columns Suffix**: Add a suffix to probability columns for easy identification.
-
-    ### Output:
-
-    - **Predicted Labels Table**: A table containing image predictions with the assigned class label.
-    - **Class Probability Estimates (Optional)**: If enabled, displays confidence scores for each class.
 
     ### How It Works:
 
@@ -480,7 +438,7 @@ class VisionTransformerPredictor:
     3. The highest probability class is assigned as the predicted label.
     4. (Optional) Class probability estimates are computed using softmax and included in the output.
 
-    
+
     """
 
 
