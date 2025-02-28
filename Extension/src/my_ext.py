309f9bb9b62c1efe2fde82de3b38fde3ed74a558
Alberto de Leo <deleoa@tcd.ie>
2025-02-28 09:17:00 +0000

    changes

--- Extension/src/my_ext.py
+++ Extension/src/my_ext.py
@@ -4,10 +4,9 @@ import knime.extension as knext
 main_category = knext.category(
     path="/community/",
     level_id="vit_ft",
-    name="Vision Transformers and Flickr Images Retrieval",
-    description="Nodes for Vision Transformers and Flickr Images Retrieval",
+    name="Vision Transformers",
+    description="Nodes for fine-tuning Vision Transformers",
     icon="icons/icon.png",
 )
 
 from nodes import vit
-from nodes import flickr_image_downloader
